bca65798516122ada783765e4946b3abbe45c7e0
51151 <jtning@bupt.edu.cn>
2021-09-27 14:59:18 +0800

    dataset

--- DataPre.py
+++ DataPre.py
@@ -1,2 +1,10 @@
-def readflie(filename):
-    fliepath = './CPA'
+import pandas as pd
+
+def readflie(pathname):
+    filepath = './CPA测试数据/'
+    plaintext = []
+    plaintext_data = pd.read_csv(filepath+pathname+'/Plaintext.csv')
+    print(plaintext_data)
+
+if __name__ == '__main__':
+    readflie('相关能量分析_原始波_无滤波_100条_8500点')
