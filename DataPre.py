c2f4e315c03c22416b46c4560caed8c70561aac4
51151 <jtning@bupt.edu.cn>
2021-09-27 16:02:30 +0800

    dataset

--- DataPre.py
+++ DataPre.py
@@ -2,22 +2,27 @@ import pandas as pd
 
 
 def num2str(num):
-    if num < 10:
-        return '00' + str(num)
-    if num < 100:
-        return '0' + str(num)
+    if num < 9:
+        return '00' + str(num+1)
+    if num < 99:
+        return '0' + str(num+1)
     else:
-        return str(num)
+        return str(num+1)
 
 
 def readfile(pathname, n=100):  # pathname为具体文件夹名，n为波形条数，默认100条
     filepath = './CPA测试数据/' + pathname
-    plaintext = []
 
     plaintext_data = pd.read_csv(filepath + '/Plaintext.csv', header=None)
     plaintext = plaintext_data[(plaintext_data.index + 2) % 4 == 0]
     pt_list = plaintext.values.tolist()
     plaintext.to_csv('plaintext_raw.csv',index=False,header=False)
+
+    trace = []
+    for i in range(n):
+        trace_data = pd.read_csv(filepath+'/Trace000'+num2str(i)+'.csv', header=None)
+        trace.append(trace_data[0].values.tolist())
+
 
 if __name__ == '__main__':
     readfile('相关能量分析_原始波_无滤波_100条_8500点')
